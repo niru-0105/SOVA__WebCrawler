9be43992c3460cf31223e31f5a4865b1f5987fdb
niru-0105 <84663564+niru-0105@users.noreply.github.com>
2022-01-10 20:02:20 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,83 +1,64 @@
+
 import request from 'request';
 import cheerio from 'cheerio';
-import chalk from 'chalk'
+import chalk from 'chalk';
 
 const inputurl = process.argv[2];
-
 const allLinks = [];
 
-let urlstr = inputurl;
 
-
-
-
-const gettingDomain = u=>{
-    if(u.substring(0,8)=='https://')
-    {
+const gettingDomain = u => {
+    if (u.substring(0, 8) == 'https://') {
         u = u.substring(8);
     }
-    if(u.substring(0,7)=='http://')
-    {
+
+    if (u.substring(0, 7) == 'http://') {
         u = u.substring(7);
     }
-    for(let i = 0; i<u.length;i++)
-    {
-        if(u[i]=='/')
-        {
-            u = u.substring(0,i);
+
+    for (let i = 0; i < u.length; i++) {
+        if (u[i] == '/') {
+            u = u.substring(0, i);
             break;
         }
     }
-    
+
     return u;
-    
+
 }
 
-console.log(chalk.green("Greetings from"),chalk.green.underline.bold("SOVA_WebCrawler"));
+console.log(chalk.green("Greetings from"), chalk.green.underline.bold("SOVA_WebCrawler"));
 console.log(chalk.hex('#DEADff').bold('Please wait while we are crawling your URL'));
-const inputDomain = gettingDomain(inputurl)
-urlstr = 'https://'+gettingDomain(urlstr);
+
+
+let urlstr = 'https://' + gettingDomain(inputurl);
 request(urlstr, cb);
-
-
-
-
 function cb(error, response, html) {
     if (error) {
         console.error(chalk.red('Error occuured! please enter valid URL or check your internet connection')); // Print the error if one occurred
     } else {
         handlehtml(html);
-        // Print the HTML for the Google homepage.
     }
 }
 
 function handlehtml(html) {
     let $ = cheerio.load(html);
     let a = $("a");
-    //console.log($(a[0]).text());
-    for(let i = 0; i<a.length; i++){
+    for (let i = 0; i < a.length; i++) {
         let urltext = $(a[i]).text();
         let href = $(a[i]).attr('href');
-        
-        if(urltext!="")
-        {
-            // if(href.substring(0,4)!=='http')
-            // {
-            //     href = "https://"+url.hostname + $(a[i]).attr('href')
-            // }
-            allLinks.push({text:urltext,href:href})
+
+        if (urltext != "") {
+            allLinks.push({ text: urltext, href: href })
         }
-        //console.log( $(a[i]).text(),$(a[i]).attr('href'));
     }
-    
-    // console.log(allLinks[2]['text']);
-    console.log(`${chalk.yellowBright('Domain name of your URL is')} ${chalk.yellow.underline.bold(inputDomain)}`);
+
+
+    console.log(`${chalk.yellowBright('Domain name of your URL is')} ${chalk.yellow.underline.bold(gettingDomain(inputurl))}`);
     const numoflinks = allLinks.length;
     console.log(`We have found ${numoflinks} on the hmoe page of URL ${inputurl} `);
-    for(a of allLinks){
-        // console.log(`Protocol: ${a['href'].substring(0,5)}`,gettingDomain(inputurl),a['href'].substring(8+inputDomain.length));
-
-        console.log(a['text'],chalk.hex('#DEADED').bold.underline(`${a['href']}`));
+    for (a of allLinks) {
+        console.log(a['text'], chalk.hex('#DEADED').bold.underline(`${a['href']}`));
     }
-       
+
 }
